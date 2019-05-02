b19aed1b64fc08d73850f057b0da81bb0557fecd
Mikal <38553900+2003mi@users.noreply.github.com>
2019-05-02 21:18:45 +0200

    media player beta"
    this is the beta version more styling adn functions will come later

--- music/imp.js
+++ music/imp.js
@@ -1,6 +1,13 @@
 var music = new Audio;
 var song = "";
 var playing = 0;
+var slider = document.getElementById("myRange");
+var sliders = document.getElementById("my1Range");
+var output = document.getElementById("demo");
+var minutes = 0;
+var seconds = 0;
+
+output.innerHTML = slider.value;
 
 function pause() {
     music.pause();
@@ -11,7 +18,7 @@ function play(musi) {
     if (musi == song) {
         if (playing == 0) {
             music.play();
-            playing = 1;
+            playing = 1;            
         }
     } else if (musi != song) {
         music.pause();
@@ -19,5 +26,46 @@ function play(musi) {
         music.play();
         song = musi;
         playing = 1;
+        music.volume = slider.value/100;
+
+
     }
 }
+
+slider.oninput = function() {
+    music.volume = this.value/100;
+    output.innerHTML = this.value;
+}
+
+sliders.oninput = function() {
+    music.currentTime = (this.value*music.duration)/10000;
+    if(playing == 0){
+        music.play();
+        music.currentTime = (this.value*music.duration)/10000;
+        playing = 1;
+    }
+}
+function str_pad_left(string,pad,length) {
+    return (new Array(length+1).join(pad)+string).slice(-length);
+}
+
+var x = setInterval(function () {
+    if (song != ""){
+    document.getElementById("my1Range").value = (music.currentTime * 10000)/music.duration;
+
+    minutes = Math.floor(music.currentTime/ 60);
+    seconds = Math.floor(music.currentTime) - (minutes*60);
+    document.getElementById("tipl").innerHTML = str_pad_left(minutes,'0',2)+':'+str_pad_left(seconds,'0',2);
+
+    minutes = Math.floor(music.duration/ 60);
+    seconds = Math.floor(music.duration) - (minutes*60);
+    document.getElementById("ftm").innerHTML = str_pad_left(minutes,'0',2)+':'+str_pad_left(seconds,'0',2);
+    
+}
+    if((music.currentTime * 10000)/music.duration==10000){
+        music.pause();
+        playing = 0;
+    }
+}, 100);
+
+x;
