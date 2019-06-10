140d1c65d563f7b3b5cd84db4868ea2edc9819ce
Mikal <38553900+2003mi@users.noreply.github.com>
2019-06-10 11:11:12 +0200

    begining to build control panel
    i have now mad it so you can whrite in an fourm an afterwards check if it is an valid url or not

--- sides/main.js
+++ sides/main.js
@@ -5,6 +5,10 @@ var urls = new Array(
     "https://youtu.be/bjMeiHBWns8"
 );
 
+// Get the input field // https://www.w3schools.com/howto/howto_js_trigger_button_enter.asp
+var input = document.getElementById("urlbox");
+
+
 // Gets params that was sent with the url
 url = getAllUrlParams().url;
 fetch = getAllUrlParams().fetch;
@@ -15,6 +19,7 @@ if(url != undefined) {
     console.log(url)
 
 } else if (fetch == "true"){
+    removeElement("main")
     // Gets the cookies and displays them on the wabpage
     console.log(fetch)
     document.write("<p id='load'>Fetching cookies...</p>");
@@ -22,13 +27,24 @@ if(url != undefined) {
         removeElement("load")
     }, 1000);   
 
-       // if you come from q/index.html then rederect to random page
+        // if you come from q/index.html then rederect to random page
 } else if(redirect == "true") {
+    removeElement("main")
     // Gets an random items from either stock urls or the cookies that is stored.
     var random = Math.floor(Math.random()*urls.length);
     // delets main and replazes it with url that will be rederected to.
-    removeElement("main")
+    //removeElement("main")
     document.write("<a href=", urls[random], ">", urls[random], "</a>", "<br>");
     window.location.href = urls[random];
     window.location.replace(urls[random]);
+} else {
+
 }
+
+function submit(){
+    var link = urlbox.value;
+    var getvalid = validURL(link);
+    console.log(getvalid);
+    document.getElementById("txt").innerHTML = link + " is " + getvalid;
+};
+
